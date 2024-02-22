bf7d42bcca28b71f3e497858af6acb92faf2e180
MiguelCM03 <miguelangelcanomorales191@gmail.com>
2024-02-22 11:41:25 +0100

    Recibe el nombre, lo guarda y muestra toda la conversacion a los usuarios nuevos

--- src/util/ClienteMulticast.java
+++ src/util/ClienteMulticast.java
@@ -1,6 +1,10 @@
 package util;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.*;
 import java.util.Scanner;
 
@@ -11,26 +15,38 @@ public class ClienteMulticast {
             int puertoServer = 33333;
             DatagramSocket socketCliente = new DatagramSocket();
             byte[] buffer;
-            DatagramPacket pqt;
+            byte[] bufferNombre;
+            DatagramPacket pqtSalida;
+            DatagramPacket pqtNombre;
+
             Scanner tcd = new Scanner(System.in);
             System.out.println("Dime tu nombre: ");
-            String nombre = tcd.nextLine();
-            buffer = nombre.getBytes();
-            pqt = new DatagramPacket(buffer, buffer.length, direccionServer, puertoServer);
-            socketCliente.send(pqt);
-            while(true){
-                String mensaje = tcd.nextLine();
+            String nombre = "nombre" + tcd.nextLine();
+            bufferNombre = nombre.getBytes();
+            pqtNombre = new DatagramPacket(bufferNombre, bufferNombre.length, direccionServer, puertoServer);
+            socketCliente.send(pqtNombre);
+            System.out.println("-INICIO DEL CHAT-");
+            System.out.println("-----------------");
+
+            boolean fin = false;
+            while(!fin){
+                String mensaje = "mensaje" + tcd.nextLine();
+                if(mensaje.equalsIgnoreCase("mensajefin")){
+                    fin = true;
+                }
+
                 buffer = mensaje.getBytes();
-                pqt  = new DatagramPacket(buffer, buffer.length, direccionServer, puertoServer);
-                socketCliente.send(pqt);
+                pqtSalida = new DatagramPacket(buffer, buffer.length, direccionServer, puertoServer);
+                socketCliente.send(pqtSalida);
 
                 byte[] bufferEntrada = new byte[1024];
-                DatagramPacket entrada = new DatagramPacket(bufferEntrada, bufferEntrada.length);
-                socketCliente.receive(entrada);
-                String respuesta = new String(entrada.getData()).trim();
+                DatagramPacket pqtEntrada = new DatagramPacket(bufferEntrada, bufferEntrada.length);
+                socketCliente.receive(pqtEntrada);
+                String respuesta = new String(pqtEntrada.getData()).trim();
                 System.out.println(respuesta);
 
             }
+            System.exit(1);
 
         } catch (UnknownHostException e) {
             throw new RuntimeException(e);
--- src/util/ServidorMulticast.java
+++ src/util/ServidorMulticast.java
@@ -11,12 +11,12 @@ import java.util.Map;
 public class ServidorMulticast {
     public static void main(String[] args) {
         try {
+            String conversacion = "";
             int puerto = 33333;
             InetAddress grupo= InetAddress.getByName("225.0.0.1");
             MulticastSocket socketServidor = new MulticastSocket(puerto);
             byte[] bufferEntrada = new byte[1024];
-            DatagramPacket entrada = new DatagramPacket(bufferEntrada, bufferEntrada.length);
-            DatagramPacket pqtNombre = new DatagramPacket(bufferEntrada, bufferEntrada.length);
+            DatagramPacket pqtEntrada = new DatagramPacket(bufferEntrada, bufferEntrada.length);
             Map<String, Integer> mapa = new HashMap<>();
 
             socketServidor.joinGroup(grupo);
@@ -27,28 +27,40 @@ public class ServidorMulticast {
 
 
             while(true){
-                socketServidor.receive(pqtNombre);
-                String nombreRecibido = new String(entrada.getData()).trim();
-                Integer puertoCliente = pqtNombre.getPort();
-                if(!mapa.containsKey(nombreRecibido)){
-                    mapa.put(nombreRecibido, puertoCliente);
-                }
-
-
-                System.out.println("Dirección: " + pqtNombre.getAddress());
-                System.out.println("Puerto: " + pqtNombre.getPort());
-
-
-                socketServidor.receive(entrada);
-                String recibido = new String(entrada.getData()).trim();
-                System.out.println(recibido);
-
-                for (Map.Entry<String, Integer> entry : mapa.entrySet()){
-                    String respuesta = entry.getKey() + ": " + recibido;
-                    bufferSalida = respuesta.getBytes();
-                    salida = new DatagramPacket(bufferSalida, bufferSalida.length, direccionCliente, entry.getValue());
+                socketServidor.receive(pqtEntrada);
+                String recibido = new String(pqtEntrada.getData(),0,pqtEntrada.getLength()).trim();
+                if(recibido.startsWith("nombre")){
+                    Integer puertoCliente = pqtEntrada.getPort();
+                    String nombreRecibido = "";
+                    for(int i = 6; i < recibido.length(); i++){
+                        nombreRecibido += recibido.charAt(i);
+                    }
+                    if(!mapa.containsKey(nombreRecibido)){
+                        mapa.put(nombreRecibido, puertoCliente);
+                    }
+                    bufferSalida = conversacion.getBytes();
+                    int puertoUsuarioActual = pqtEntrada.getPort();
+                    salida = new DatagramPacket(bufferSalida, bufferSalida.length, direccionCliente, puertoUsuarioActual);
                     socketServidor.send(salida);
+
+
+                    System.out.println("Dirección: " + pqtEntrada.getAddress());
+                    System.out.println("Puerto: " + pqtEntrada.getPort());
+                }else if(recibido.startsWith("mensaje")){
+                    String mensajeRecibido = "";
+                    for(int i = 7; i < recibido.length(); i++){
+                        mensajeRecibido += recibido.charAt(i);
+                    }
+                    conversacion += "\n" + mensajeRecibido;
+                    for (Map.Entry<String, Integer> entry : mapa.entrySet()){
+                        String respuesta = entry.getKey() + ": " + mensajeRecibido;
+                        bufferSalida = respuesta.getBytes();
+                        salida = new DatagramPacket(bufferSalida, bufferSalida.length, direccionCliente, entry.getValue());
+                        socketServidor.send(salida);
+                    }
                 }
+
+
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
